165db0081c1b181767a9008ce5d42d96b5ba6254
Rajat1agva <88077000+Rajat1agva@users.noreply.github.com>
2021-07-29 11:40:05 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,27 @@
 
+
+#define AMP 5 
+#define TIME_PERIOD 10
+float output=0;
+float time=0;
+#define frequency (1/)
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode(9, OUTPUT);
+Serial.begin(9600);
+frequency=
+
+}
+
+void loop() {
+  
+   t=t+0.0000000625; //TIME IN SECONDS IF SYSTEM CLOCK IS 16 MHZ
+  
+  // put your main code here, to run repeatedly:
+
+}
+void sinwave(float Amp,)
+{
+Serial.println(sin(2*100*PI*t));
+}
