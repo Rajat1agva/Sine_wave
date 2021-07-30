e793115ad77f6ae0a8aa8ba373a64ca927022e9a
Rajat1agva <88077000+Rajat1agva@users.noreply.github.com>
2021-07-30 15:01:18 +0530

    Update main.ino

--- main.ino
+++ main.ino
@@ -1,35 +1,41 @@
-
-#define AMP 5
+#define ledpin 13
+#define AMPLITUDE 5   //Amplitude value
+#define AMP ((AMPLITUDE*255)/5)//
+#define frequency 0.1 //frequency of sine wave
 float output = 0;
 int miliseconds = 0;
-#define frequency 0.1
-//bool state=false;
+float amp_value=0;
+
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(9, OUTPUT);
+  pinMode(ledpin, OUTPUT);
   Serial.begin(115200);
-
-
+  
 }
 
 void loop() {
 
-  // time = time + 0.00005; //TIME IN SECONDS IF SYSTEM CLOCK IS 16 MHZ
-  sinwave();
+ sinwave();
  
-  // put your main code here, to run repeatedly:
-
 }
 void sinwave(void)
 {
   miliseconds = millis();
   int t = 0;
   while (t < 10000)
-  {
-    Serial.println(5 * sin(2 * PI * frequency * t * 0.001));
+  { output=AMP* sin(2 * PI * frequency * t * 0.001);
+     Serial.println(output);
+    if(output>=0)
+    {
+      analogWrite(ledpin,output);
+    }
+    else
+    {
+      analogWrite(ledpin,0);
+    }
     t = millis() - miliseconds;
-   // Serial.println();
+   
   }
 
   miliseconds = 0;
