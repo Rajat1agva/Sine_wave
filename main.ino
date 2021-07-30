41498c392d89c68a370115e39f260c3f383ed1e1
Rajat1agva <88077000+Rajat1agva@users.noreply.github.com>
2021-07-30 14:35:05 +0530

    Update main.ino

--- main.ino
+++ main.ino
@@ -1,35 +1,37 @@
 
-#define AMP 5 
-#define TIME_PERIOD 0.01
-float output=0;
-float time=0;
-#define frequency (1/TIME_PERIOD)
+#define AMP 5
+float output = 0;
+int miliseconds = 0;
+#define frequency 0.1
+//bool state=false;
 
 void setup() {
   // put your setup code here, to run once:
-pinMode(9, OUTPUT);
-Serial.begin(115200);
+  pinMode(9, OUTPUT);
+  Serial.begin(115200);
 
 
 }
 
 void loop() {
-  
-   time=time+0.000000625; //TIME IN SECONDS IF SYSTEM CLOCK IS 16 MHZ
-   sinwave();
+
+  // time = time + 0.00005; //TIME IN SECONDS IF SYSTEM CLOCK IS 16 MHZ
+  sinwave();
+ 
   // put your main code here, to run repeatedly:
 
 }
 void sinwave(void)
 {
-output=(AMP*sin(2*frequency*PI*time));
-if(output>=0)
-{
-  Serial.println(output);
+  miliseconds = millis();
+  int t = 0;
+  while (t < 10000)
+  {
+    Serial.println(5 * sin(2 * PI * frequency * t * 0.001));
+    t = millis() - miliseconds;
+   // Serial.println();
+  }
 
-}
-else
-{
-   Serial.println(0);
-}
+  miliseconds = 0;
+
 }
